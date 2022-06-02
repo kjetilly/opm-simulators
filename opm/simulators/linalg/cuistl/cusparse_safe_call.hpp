7915674afd747abd3ee9ffb5a3d2fb0e6d71a9c6
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-06-02 15:11:58 +0200

    fixes to cusparse safe call

--- opm/simulators/linalg/cuistl/cusparse_safe_call.hpp
+++ opm/simulators/linalg/cuistl/cusparse_safe_call.hpp
@@ -5,7 +5,7 @@
 #include <opm/common/ErrorMacros.hpp>
 #include <opm/common/OpmLog/Logger.hpp>
 #include <sstream>
-#define ADD_CUSPARSE_ERROR_TYPE(x) errorMessages[x] = #x
+#define ADD_CUSPARSE_ERROR_TYPE(x) errorMessages[int(x)] = #x
 namespace
 {
 inline std::string
@@ -13,18 +13,18 @@ getCusparseErrorMessage(int code)
 {
     std::map<int, std::string> errorMessages;
 
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_SUCCESS);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_NOT_INITIALIZED);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_ALLOC_FAILED);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_INVALID_VALUE);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_ARCH_MISMATCH);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_MAPPING_ERROR);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_EXECUTION_FAILED);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_INTERNAL_ERROR);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_ZERO_PIVOT);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_NOT_SUPPORTED);
-    ADD_CUSPRASE_ERROR_TYPE(CUSPARSE_STATUS_INSUFFICIENT_RESOURCES);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_SUCCESS);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_NOT_INITIALIZED);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_ALLOC_FAILED);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_INVALID_VALUE);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_ARCH_MISMATCH);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_MAPPING_ERROR);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_EXECUTION_FAILED);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_INTERNAL_ERROR);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_ZERO_PIVOT);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_NOT_SUPPORTED);
+    ADD_CUSPARSE_ERROR_TYPE(CUSPARSE_STATUS_INSUFFICIENT_RESOURCES);
     if (errorMessages.find(code) != errorMessages.end()) {
         return errorMessages.at(code);
     } else {
@@ -41,7 +41,7 @@ getCusparseErrorMessage(int code)
             message << "cuSparse expression did not execute correctly. Expression was: \n";                            \
             message << "    " << #expression << "\n";                                                                  \
             message << "in function " << __func__ << ", in " << __FILE__ << " at line " << __LINE__ << "\n";           \
-            message << "CuSparse error code was: " << getCusparseErrorMessage(code) << "\n";                           \
+            message << "CuSparse error code was: " << getCusparseErrorMessage(error) << "\n";                          \
             OpmLog::error(message.str());                                                                              \
             OPM_THROW(std::runtime_error, message.str());                                                              \
         }                                                                                                              \
