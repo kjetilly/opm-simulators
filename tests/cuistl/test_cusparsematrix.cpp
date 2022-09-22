7d407128b2ccd5e90bf11d46772853b44f461ce0
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-09-22 08:31:18 +0200

    added test for random matrix

--- tests/cuistl/test_cusparsematrix.cpp
+++ tests/cuistl/test_cusparsematrix.cpp
@@ -8,6 +8,7 @@
 #include <opm/simulators/linalg/cuistl/CuVector.hpp>
 #include <opm/simulators/linalg/cuistl/PreconditionerAdapter.hpp>
 #include <opm/simulators/linalg/cuistl/cuda_safe_call.hpp>
+#include <random>
 
 BOOST_AUTO_TEST_CASE(TestConstruction1D)
 {
@@ -85,86 +86,79 @@ BOOST_AUTO_TEST_CASE(TestConstruction1D)
 }
 
 
-BOOST_AUTO_TEST_CASE(TestApply2D)
+BOOST_AUTO_TEST_CASE(RandomSparsityMatrix)
 {
+    std::srand(0);
+    double nonzeroPercent = 0.2;
+    std::mt19937 generator;
+    std::uniform_real_distribution<double> distribution(0.0,1.0);
     constexpr size_t dim = 3;
-    const int N = 5;
-    const int nonZeroes = N * 3 - 2;
+    const int N = 300;
     using M = Dune::FieldMatrix<double, dim, dim>;
     using SpMatrix = Dune::BCRSMatrix<M>;
     using Vector = Dune::BlockVector<Dune::FieldVector<double, dim>>;
 
+    
+    std::vector<std::vector<size_t>> nonzerocols(N);
+    int nonZeroes = 0;
+    for (auto row = 0; row < N; ++row) {
+        for (size_t col = 0; col < N; ++col) {
+            if (distribution(generator) < nonzeroPercent) {
+                nonzerocols.at(row).push_back(col);
+                nonZeroes++;
+            }
+        }
+    }
     SpMatrix B(N, N, nonZeroes, SpMatrix::row_wise);
     for (auto row = B.createbegin(); row != B.createend(); ++row) {
-        // Add nonzeros for left neighbour, diagonal and right neighbour
-        if (row.index() > 0) {
-            row.insert(row.index() - 1);
-        }
-        row.insert(row.index());
-        if (row.index() < B.N() - 1) {
-            row.insert(row.index() + 1);
+        for (size_t j = 0; j < nonzerocols[row.index()].size(); ++j) {
+            row.insert(nonzerocols[row.index()][j]);
         }
     }
     // This might not be the most elegant way of filling in a Dune sparse matrix, but it works.
-
-    auto index = [&](size_t i, size_t j, size_t c1, size_t c2) {
-        return i * N * N * dim * dim * dim * dim + j * N * dim * dim + c1 * dim  + c2;
-    };
     for (int i = 0; i < N; ++i) {
-        if (i < N - 1) {
+        for (size_t j = 0; j < nonzerocols[i].size(); ++j) {
             for (size_t c1 = 0; c1 < dim; ++c1) {
                 for (size_t c2 = 0; c2 < dim; ++c2) {
-                    B[i][i + 1][c1][c2] = index(i, i + 1, c1, c2);
-                }
-            }
-        }
-        for (size_t c1 = 0; c1 < dim; ++c1) {
-            for (size_t c2 = 0; c2 < dim; ++c2) {
-                    B[i][i][c1][c2] = index(i, i, c1, c2);
-            }
-        }
-       
-        if (i > 0) {
-            for (size_t c1 = 0; c1 < dim; ++c1) {
-                for (size_t c2 = 0; c2 < dim; ++c2) {
-                    B[i][i - 1][c1][c2] = index(i, i - 1, c1, c2);
+                    B[i][nonzerocols[i][j]][c1][c2] = distribution(generator);
                 }
             }
         }
     }
 
     auto cuSparseMatrix = Opm::cuistl::CuSparseMatrix<double>::fromMatrix(B);
-    std::vector<double> inputDataX(N * dim, 1.0);
-    for (size_t i =  0; i < inputDataX.size(); ++i)  {
-        inputDataX[i] =  i;
-    }
-    std::vector<double> inputDataY(N * dim, .25);
-    auto inputVectorX = Opm::cuistl::CuVector<double>(inputDataX.data(), inputDataX.size());
-    auto inputVectorY = Opm::cuistl::CuVector<double>(inputDataY.data(), inputDataY.size());
-    Vector xHost(N), yHost(N);
-    yHost = inputDataY[0];
-    inputVectorX.copyTo(xHost); 
-    const double alpha = 1.42;
-    cuSparseMatrix.usmv(alpha, inputVectorX, inputVectorY);
+    // check each column
+    for (size_t component = 0; component < N; ++component) {
+        std::vector<double> inputDataX(N * dim, 0.0);
+        inputDataX[component] = 1.0;
+        std::vector<double> inputDataY(N * dim, .25);
+        auto inputVectorX = Opm::cuistl::CuVector<double>(inputDataX.data(), inputDataX.size());
+        auto inputVectorY = Opm::cuistl::CuVector<double>(inputDataY.data(), inputDataY.size());
+        Vector xHost(N), yHost(N);
+        yHost = inputDataY[0];
+        inputVectorX.copyTo(xHost); 
+        const double alpha = 1.42;
+        cuSparseMatrix.usmv(alpha, inputVectorX, inputVectorY);
 
-    inputVectorY.copyToHost(inputDataY);
-    
-    B.usmv(alpha, xHost, yHost);
-    for (size_t i = 0; i < N;  ++i) {
-        for (size_t c  = 0; c < dim; ++c)  {
-            BOOST_CHECK_CLOSE(inputDataY[i*dim +c], yHost[i][c], 1e-7);
+        inputVectorY.copyToHost(inputDataY);
+        
+        B.usmv(alpha, xHost, yHost);
+        for (size_t i = 0; i < N;  ++i) {
+            for (size_t c  = 0; c < dim; ++c)  {
+                BOOST_CHECK_CLOSE(inputDataY[i*dim +c], yHost[i][c], 1e-7);
+            }
         }
-    }
-    inputVectorX.copyTo(xHost);
+        inputVectorX.copyTo(xHost);
 
-    cuSparseMatrix.mv(inputVectorX, inputVectorY);
+        cuSparseMatrix.mv(inputVectorX, inputVectorY);
 
-    inputVectorY.copyToHost(inputDataY);
-    
-    B.mv(xHost, yHost);
-    for (size_t i = 0; i < N;  ++i) {
-        for (size_t c  = 0; c < dim; ++c)  {
-            BOOST_CHECK_CLOSE(inputDataY[i*dim +c], yHost[i][c], 1e-7);
+        inputVectorY.copyToHost(inputDataY);
+        
+        B.mv(xHost, yHost);
+        for (size_t i = 0; i < N;  ++i) {
+            for (size_t c  = 0; c < dim; ++c)  {
+                BOOST_CHECK_CLOSE(inputDataY[i*dim +c], yHost[i][c], 1e-7);
+            }
         }
     }
 }
