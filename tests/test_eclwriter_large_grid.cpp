a8dbc0b45f52b40944c9aea4010ee0ec06f2cceb
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-08-07 09:51:05 +0200

    Some formatting fixes

--- tests/test_eclwriter_large_grid.cpp
+++ tests/test_eclwriter_large_grid.cpp
@@ -1,20 +1,20 @@
 /*
-  Copyright 2023 SINTEF Digital, Mathematics and Cybernetics.
+  Copyright 2024 SINTEF Digital, Mathematics and Cybernetics.
 
-This file is part of the Open Porous Media project (OPM).
+  This file is part of the Open Porous Media project (OPM).
 
-OPM is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-OPM is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <config.h>
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_CASE(TestWrite)
 {
     const auto makeFieldname = [](size_t i) { return fmt::format("S{:03d}", i); };
 
-    const size_t size = 466 * 466 * 466;
+    const size_t size = 32; // 466 * 466 * 466;
     const size_t numberOfOutputs = 128;
 
     const auto makeFieldData = [](size_t i, size_t j) { return i * size + j; };
@@ -44,7 +44,7 @@ BOOST_AUTO_TEST_CASE(TestWrite)
 
 
         for (size_t i = 0; i < numberOfOutputs; ++i) {
-            std::cout << "#";
+            std::cout << fmt::format("{:04d}\r", i) << std::flush;
             std::flush(std::cout);
             std::vector<double> dummyOutput(size, 42.0);
             for (size_t j = 0; j < size; ++j) {
